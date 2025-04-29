99f67c7ab05e6834dc46204b68451cb09fcb6e29
岩浆 <linboyan5438081@163.com>
2025-04-30 00:51:20 +0800

    feat: 串口使用扩展库支持不定长接收
    如果出现不稳定现象，或优先保证稳定性，应把接收中断由Callback移至IRQHandler，这样当串口空闲/串口达到指定字节/串口出错时都会重新启动接收中断，而非只有接收完全成功的两种情况重启中断
    HAL_UARTEx: HAL_UART扩展库
    HAL_UARTEx_ReceiveToIdle_DMA: 使用DMA，长数据时可减少中断次数，注意DMA有独立于串口的中断优先级
    HAL_UARTEx_ReceiveToIdle_IT: 使用中断，每字节中断一次，节约DMA资源
    DMA通道和流: 通道和流是一对一的关系，即不能同时使用同一个通道或流上的多个外设，一个外设(比如一个rx脚)只能绑定到一个通道的一个流上，映射关系需查对应芯片的表

--- User/Inc/debug.h
+++ User/Inc/debug.h
@@ -1,6 +1,6 @@
 #include "main.h"
 
 void debug_init(void);
-#define RX_LEN_DEBUG 1 // 定义接收缓冲区长度
+#define RX_LEN_DEBUG 3000 // 定义接收缓冲区长度
 extern char debug_rx[RX_LEN_DEBUG]; // 字符串类型接收数据缓冲区
 
--- User/Src/debug.c
+++ User/Src/debug.c
@@ -1,25 +1,28 @@
 #include "debug.h"
 #include "usart.h" 
+#include "string.h" // 字符串处理库
 
 char debug_rx[RX_LEN_DEBUG]; // 字符串类型接收数据缓冲区
 
 void debug_init(void)
 {
-	HAL_UART_Transmit(&huart1, (uint8_t *)"\rUSART1 connected!", 17, HAL_MAX_DELAY); // 开机打个招呼
-  HAL_UART_Receive_IT(&huart1, (uint8_t *)&debug_rx, sizeof(debug_rx)); // 启动接收中断
+	HAL_UART_Transmit(&huart1, (uint8_t *)"\rUSART1 connected!", 18, HAL_MAX_DELAY); // 开机打个招呼
+  HAL_UARTEx_ReceiveToIdle_IT(&huart1,(uint8_t *)debug_rx,RX_LEN_DEBUG); // 启动接收空闲中断
 }
 
 /**
  * @brief 处理 USART1 接收完成中断回调函数
  * @param huart: 指向 UART_HandleTypeDef 结构体的指针
+ * @param Size: 接收到的数据长度
  */
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size)
 {
-  if (huart->Instance == USART1) // 判断是否是 USART1 的中断请求
-  {
-    HAL_UART_Transmit(&huart1, (uint8_t *)&debug_rx, sizeof(debug_rx), HAL_MAX_DELAY); // 回显接收到的数据
-    HAL_UART_Receive_IT(&huart1, (uint8_t *)&debug_rx, sizeof(debug_rx)); // 重新启动接收中断
-  }
+    if (huart->Instance == USART1)
+    {
+        HAL_UART_Transmit(&huart1, (uint8_t *)debug_rx, Size, HAL_MAX_DELAY);	//将接受到的数据再发回上位机
+        memset(debug_rx, 0, Size);
+        HAL_UARTEx_ReceiveToIdle_IT(&huart1,(uint8_t *)debug_rx,RX_LEN_DEBUG); // 启动接收中断
+    }
 }
 
 /**
