abc58edec67031b19ee11026b71f32187cb6ca72
岩浆 <linboyan5438081@163.com>
2025-04-30 00:52:35 +0800

    Revert "feat(CubeMX): 启用uart1rx的DMA"
    This reverts commit 30cd319872149e1fa6bb78a549ff6271a9979c81.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "dma.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -89,7 +88,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_USART1_UART_Init();
 
   /* Initialize interrupts */
