d93c2924ddc408e4992b3fdd9af75ada99860bce
岩浆 <linboyan5438081@163.com>
2025-06-01 18:01:52 +0800

    fix: 接线变更

--- User/Src/l298n.c
+++ User/Src/l298n.c
@@ -5,16 +5,16 @@ void l298n_set(uint8_t cmd);
 void l298n_set(uint8_t cmd)
 {
   // 第一块L298N模块的INA~D
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2, (cmd & (0x01 << 7)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, (cmd & (0x01 << 6)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, (cmd & (0x01 << 5)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_5, (cmd & (0x01 << 4)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, (cmd & (0x01 << 7)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, (cmd & (0x01 << 6)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, (cmd & (0x01 << 5)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_10, (cmd & (0x01 << 4)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
 
   // 第二块L298N模块的INA~D
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_3, (cmd & (0x01 << 3)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_4, (cmd & (0x01 << 2)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_5, (cmd & (0x01 << 1)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(GPIOF, GPIO_PIN_6, (cmd & (0x01 << 0)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_11, (cmd & (0x01 << 3)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_12, (cmd & (0x01 << 2)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_13, (cmd & (0x01 << 1)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_14, (cmd & (0x01 << 0)) ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
 
 void l298n_stop(void)
