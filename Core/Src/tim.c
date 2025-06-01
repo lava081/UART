1639c9b2d78ede0f2c6b41895b340e4f19c419fd
zawaluduuo <2728348659@qq.com>
2025-06-01 20:20:57 +0800

    feat(CubeMX): pwm输出应为开漏以适应上拉
    我猜应该是这样，但愿能保护单片机吧

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -220,7 +220,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PA3     ------> TIM2_CH4
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF1_TIM2;
